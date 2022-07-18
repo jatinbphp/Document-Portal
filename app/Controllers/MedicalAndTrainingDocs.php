b543da5291de8680c14552580424c9b61bdfb974
Jatin Bhatt <jatin.b.php@gmail.com>
2022-07-18 11:17:31 +0530

    Changes in medical and training docs module

--- app/Controllers/MedicalAndTrainingDocs.php
+++ app/Controllers/MedicalAndTrainingDocs.php
@@ -90,13 +90,8 @@ class MedicalAndTrainingDocs extends BaseController
     
     
 		public function view($id=""){
-			//echo $id;
-			//exit;
 			$userId = $_SESSION['id'];
-			//echo $userId;
-			//exit;
             $this->data['page_title'] = 'Medical and Training Docs';
-            
             $this->data['company_id'] = $id;
             $this->render_user_template('medicalAndTrainingDocs/view', $this->data);
 		}
@@ -107,19 +102,12 @@ class MedicalAndTrainingDocs extends BaseController
 		
 		
 	public function fetch_technician_doc($id = null){
-		
-
         $sesVal =  $_SERVER['HTTP_REFERER'];
-       // echo $sesVal;
-        //exit;
         $ses_id = explode("/",$sesVal);
-
         $comp_id = end($ses_id);
-
         $session = session();
         $logged_in_sess = [
-            'company_id' => $comp_id
-                   
+            'company_id' => $comp_id                   
         ]; 
         $this->session->set($logged_in_sess);    
         $db = \Config\Database::connect();		
@@ -186,7 +174,7 @@ class MedicalAndTrainingDocs extends BaseController
         $fetch_data = $model_user->make_datatables( $selectColumn,$whereEqual,$whereNotEqual,$orderColumn,$orderBy,$searchColumn,$joinTableArray,$notIn,$whereUser);
       
         //echo "<pre>";
-        //print_r($fetch_data);
+        //gprint_r($fetch_data);
         //exit;
      	
         $data = array();
@@ -296,131 +284,6 @@ class MedicalAndTrainingDocs extends BaseController
 
         echo json_encode($output);
         
-    }
-		
-    
-    	public function fetch_documents(){
-		$db = \Config\Database::connect();		
-  	 	$global_tblDocuments = 'DocumentsManage';
- 	  	$global_tblusers = 'Users';
-	  	$global_tblcategory = 'category';
-	  	$global_tblsubcategory = 'SubCategory';
-	  	$global_tblcompany = 'Company';
-
-        $doc_model = new DocumentsModel;
-        $existuser = $doc_model->where('userID',$_SESSION['id'])->findall();
-
-        $com_model = new UserCompanyModel;
-        $ComapanyId = $com_model->where('user_id',$_SESSION['id'])->findAll();
-
-        foreach($ComapanyId as $CompValue){
-            $CompArr[] = $CompValue['company_id'];
-        }
-
-        $ComID = implode(",",$CompArr );
-        $ComID1 = ''.$ComID.',0';
-       $CompArr1[] = $ComID1;
-
-       $IdArr = array($_SESSION['id'],0);
-       $userID = implode(",",$IdArr);
-       $userID1 = ''.$userID.'';
-       $userIDArr1[] = $userID1;
-       
-
-        
-        // equal condition
-	  	 $whereEqual=array();
-          $whereIn = array(); 
-         $orwhere=array();
-         $whereUser = array();
-         if(count($existuser)>0){
-           $whereEqual =array();
-           $whereUser[$global_tblDocuments.'.userID'] = $userIDArr1;
-            
-           //$orwhere[$global_tblDocuments.'.userID']= 0;
-         }
-         else{
-           $whereEqual[$global_tblDocuments.'.userID']= 0;
-         }
-	  
-
-
-        // not equal condition
-        $whereNotEqual = array();
-        $whereNotEqual[$global_tblDocuments.'.isActive']= 0;
-        $notIn = array();   
-       
-        $whereIn[$global_tblDocuments.'.companyID'] = $CompArr1;   
-
-        // select data
-        $selectColumn[$global_tblDocuments.'.*'] = $global_tblDocuments.'.*';
-        $selectColumn[$global_tblusers.'.firstName'] =  $global_tblusers.'.firstName';
-        $selectColumn[$global_tblusers.'.lastName'] =  $global_tblusers.'.lastName';
-        $selectColumn[$global_tblcategory.'.categoryName'] =  $global_tblcategory.'.categoryName';
-        $selectColumn[$global_tblsubcategory.'.SubCatName'] =  $global_tblsubcategory.'.SubCatName';
-        $selectColumn[$global_tblcompany.'.companyName'] =  $global_tblcompany.'.companyName';
-      	
-        // order column
-       $orderColumn = array('',$global_tblDocuments.".docName", $global_tblcategory.'.categoryName',$global_tblsubcategory.'.SubCatName', $global_tblDocuments.".expireDate",'');
-
-        // search column
-        $searchColumn = array($global_tblDocuments.".docName",$global_tblusers.".firstName",$global_tblusers.".lastName",$global_tblcompany.".companyName",$global_tblDocuments.".isActive");
-
-        // order by
-        $orderBy = array($global_tblDocuments.'.id' => "DESC");
-
-        // join table
-        $joinTableArray = array();
-       	$joinTableArray = array(array("joinTable"=>$global_tblusers, "joinField"=>"id", "relatedJoinTable"=>$global_tblDocuments, "relatedJoinField"=>"userID","type"=>"left"),
-       		array("joinTable"=>$global_tblcategory, "joinField"=>"id", "relatedJoinTable"=>$global_tblDocuments, "relatedJoinField"=>"categoryID","type"=>"left"),
-
-       		array("joinTable"=>$global_tblsubcategory, "joinField"=>"id", "relatedJoinTable"=>$global_tblDocuments, "relatedJoinField"=>"subCategoryID","type"=>"left"),
-
-       		array("joinTable"=>$global_tblcompany, "joinField"=>"id", "relatedJoinTable"=>$global_tblDocuments, "relatedJoinField"=>"companyID","type"=>"left")
-
-       );
-
-
-     	$model_user= new DocumentsModel;
-        $fetch_data = $model_user->make_datatables( $selectColumn,$whereEqual,$whereNotEqual,$orderColumn,$orderBy,$searchColumn,$joinTableArray,$notIn,$orwhere,$whereIn,$whereUser);
-      
-     	
-        $data = array();
-        //echo "<pre>";print_r($fetch_data);exit;
-        foreach ($fetch_data as $key => $row) {
-            $sub_array = array(); 
-            
-            //$imgSrc = base_url('assets/images/download1.png');
-            $id = $row['id'];
-            
-            $sub_array[] = '<a href = "' . base_url( '/uploads/documents/'.$row['categoryID'].'/'.$row['subCategoryID'].'/'.$row['docFile']). '" target="_blank"><i class="fa fa-file" style="font-size:36px;"></i></a>';
-            $sub_array[] = $row['docName'];  
-			$sub_array[] = $row['firstName']." ".$row['lastName'];  
-			$sub_array[] = $row['categoryName']; 
-			$sub_array[] = $row['SubCatName']; 
-			$sub_array[] = $row['companyName'];  
-			$sub_array[] = $row['expireDate']; 
-
-		 	if($row['isActive'] == 1){
-                $sub_array[] = '<span class="badge badge-success">Active</span>';
-            }else{
-                $sub_array[] = '<span class="badge badge-danger">InActive</span>';
-            }  
-
-            // $actionLink = $model_user->getActionLink('',$row['id'],'','Documents','');
-            // $sub_array[] = $actionLink;
-            $data[] = $sub_array;
-
-        } 
-        $output = array(
-            "draw" =>  $_POST["draw"] ,
-            "recordsTotal" => $model_user->get_all_data( $selectColumn,$whereEqual,$whereNotEqual,$orderColumn,$orderBy,$searchColumn,$joinTableArray,$notIn,$orwhere,$whereIn,$whereUser),
-            "recordsFiltered" => $model_user->get_filtered_data( $selectColumn,$whereEqual,$whereNotEqual,$orderColumn,$orderBy,$searchColumn,$joinTableArray,$notIn,$orwhere,$whereIn,$whereUser),
-            "data" => $data,
-        );
-
-        echo json_encode($output);
-    }
-    
+    }    
 }
 

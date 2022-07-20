ca172837fb9d6b289a50601cc3d390503a41a62a
Jatin Bhatt <jatin.b.php@gmail.com>
2022-07-20 13:56:33 +0530

    add code for compliance report

--- app/Controllers/CeoComplianceReportView.php
+++ app/Controllers/CeoComplianceReportView.php
@@ -125,7 +125,8 @@ class CeoComplianceReportView extends BaseController
         $subCategory = new SubCategoryModel;
         $this->data['subCategory'] = $subCategory->where('is_deleted',0)->findall();
         $documents = new WorkflowModel;
-        $this->data['Documentfiles'] = $documents->where('company_id', $com_id)->where('technician_id',$tech_id)->findAll();
+         $where =  "(is_active = 1 OR is_active = 3 OR is_active = 4)";
+        $this->data['Documentfiles'] = $documents->where('company_id', $com_id)->where('technician_id',$tech_id)->where($where)->findAll();
 
         $this->data['page_title'] = 'Compliance Report';
         $this->render_user_template('Ceo/compliance_report/report_list',$this->data);
